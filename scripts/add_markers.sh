7144f3f4807882bcf816461df0c5ccf0bc3b8840
Pongrut Palarpong <pongrut@gmail.com>
2021-02-08 19:09:28 +0700

    Update add_markers.sh

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 #xterm  -e  " roslaunch jetbot jetbot_world.launch world_file:=/home/pongrut/catkin_ws/src/worlds/pongrut.world " &
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/pongrut/catkin_ws/src/worlds/pongrut.world " &
-#xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="$PWD"/src/map/pongrut.world " &
 sleep 5
 
-#xterm  -e  " roslaunch jetbot jetbot_amcl.launch map_file:=/home/pongrut/catkin_ws/src/worlds/pongrut_map.yaml " &
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/pongrut/catkin_ws/src/worlds/pongrut_map.yaml " &
-#xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch " &
-
+#xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:="$PWD"/src/map/pongrut_map.yaml " &
+xterm  -e  " roslaunch jetbot jetbot_amcl.launch map_file:="$PWD"/src/map/pongrut_map.yaml " &
 sleep 5
-#xterm  -e  " roslaunch jetbot jetbot_view_navigation.launch "
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 
+# xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+xterm  -e  " roslaunch jetbot jetbot_view_navigation.launch " &
 sleep 15
+
 xterm -e "rosrun add_markers add_markers"
+
+
 
