c1efc6336dc11bbbee6b5da5dad7780354c5663a
Pongrut Palarpong <pongrut@gmail.com>
2021-02-08 13:52:53 +0700

    Using $PWD environment variable as the reference

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 #xterm  -e  " roslaunch jetbot jetbot_world.launch world_file:=/home/pongrut/catkin_ws/src/worlds/pongrut.world " &
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/pongrut/catkin_ws/src/worlds/pongrut.world " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="$PWD"/src/map/pongrut.world " &
 #xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
 
 #xterm  -e  " roslaunch jetbot jetbot_amcl.launch map_file:=/home/pongrut/catkin_ws/src/worlds/pongrut_map.yaml " &
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/pongrut/catkin_ws/src/worlds/pongrut_map.yaml " &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:="$PWD"/src/map/pongrut_map.yaml " &
 #xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch " &
 
 sleep 5
 #xterm  -e  " roslaunch jetbot jetbot_view_navigation.launch "
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+#xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+xterm  -geometry 80x20+0+400 -e  " roslaunch jetbot jetbot_view_navigation.launch " &
 
 sleep 15
 xterm -e "rosrun pick_objects pick_objects"
