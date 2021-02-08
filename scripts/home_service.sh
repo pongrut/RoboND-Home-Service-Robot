710a274b9134d6bd6d4a473462274a62159d75e3
Pongrut Palarpong <pongrut@gmail.com>
2021-02-08 19:04:32 +0700

    Update using $PWD ref. base directory

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # -geometry 93x31+100+350 (COLUMNSxROWS+X+Y) 
-xterm  -geometry 80x20+0+0 -e  " roslaunch jetbot jetbot_world.launch world_file:=/home/pongrut/catkin_ws/src/worlds/pongrut.world " &
+xterm  -geometry 80x20+0+0 -e  " roslaunch jetbot jetbot_world.launch world_file:="$PWD"/src/map/pongrut.world " &
 #xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/pongrut/catkin_ws/src/worlds/pongrut.world " &
 #xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
 
-xterm  -geometry 80x20+0+200 -e  " roslaunch jetbot jetbot_amcl.launch map_file:=/home/pongrut/catkin_ws/src/worlds/pongrut_map.yaml " &
+xterm  -geometry 80x20+0+200 -e  " roslaunch jetbot jetbot_amcl.launch map_file:=/"$PWD"/src/map/pongrut_map.yaml " &
 #xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/pongrut/catkin_ws/src/worlds/pongrut_map.yaml " &
 #xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch " &
 
@@ -18,5 +18,4 @@ xterm -geometry 80x20+0+600 -e "rosrun add_markers add_markers" &
 
 sleep 5
 xterm -geometry 80x20+0+800 -e "rosrun pick_objects pick_objects"
-
 
