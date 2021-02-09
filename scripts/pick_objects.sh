5d8e8dd06a8db1ea620c1490be849e297459711c
Pongrut Palarpong <pongrut@gmail.com>
2021-02-09 19:31:46 +0700

    Update pick_objects.sh

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #xterm  -e  " roslaunch jetbot jetbot_world.launch world_file:=/home/pongrut/catkin_ws/src/worlds/pongrut.world " &
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="$PWD"/src/map/pongrut.world " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="$PWD"/src/worlds/pongrut.world " &
 sleep 5
 
 #xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:="$PWD"/src/map/pongrut_map.yaml " &
