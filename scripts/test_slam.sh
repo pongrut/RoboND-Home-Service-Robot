bde04ca5ff1a5d284bc6d39d19b0deb6a6372e29
Pongrut Palarpong <pongrut@gmail.com>
2021-02-09 20:33:06 +0700

    Using argument for selecting robot

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,15 +1,21 @@
-#!/bin/sh
-#xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
-xterm  -e   " roslaunch jetbot jetbot_world.launch world_file:="$PWD"/src/worlds/pongrut.world " &
-sleep 5
+#!/bin/bash
 
-#xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
-xterm  -e  " roslaunch jetbot jetbot_gmapping.launch " &
-sleep 5
-
-#xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
-xterm  -e  " roslaunch jetbot jetbot_view_navigation.launch " &
-sleep 5
-
-#xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch "
-xterm  -e  " roslaunch jetbot teleop.launch "
+if [[ $1 == turtlebot ]]; then
+    echo "Launching turtlebot..."
+    xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+    sleep 5
+    xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+    sleep 5
+    xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+    sleep 5
+    xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch "
+else
+    echo "Launching jetbot..."
+    xterm  -e   " roslaunch jetbot jetbot_world.launch world_file:="$PWD"/src/worlds/pongrut.world " &
+    sleep 5
+    xterm  -e  " roslaunch jetbot jetbot_gmapping.launch " &
+    sleep 5
+    xterm  -e  " roslaunch jetbot jetbot_view_navigation.launch " &
+    sleep 5
+    xterm  -e  " roslaunch jetbot teleop.launch "
+fi
