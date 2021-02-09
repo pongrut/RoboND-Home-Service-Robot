4c594e6724a5af2528e865b4b27b32e4df05cd1d
Pongrut Palarpong <pongrut@gmail.com>
2021-02-09 19:32:08 +0700

    Update add_markers.sh

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #xterm  -e  " roslaunch jetbot jetbot_world.launch world_file:=/home/pongrut/catkin_ws/src/worlds/pongrut.world " &
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="$PWD"/src/map/pongrut.world " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="$PWD"/src/worlds/pongrut.world " &
 sleep 5
 
 #xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:="$PWD"/src/map/pongrut_map.yaml " &
@@ -12,6 +12,5 @@ xterm  -e  " roslaunch jetbot jetbot_view_navigation.launch " &
 sleep 15
 
 xterm -e "rosrun add_markers add_markers"
-
 
 
