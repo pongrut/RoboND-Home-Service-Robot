8d0af651a537108f38613b8da7d47d8ac041d483
Pongrut Palarpong <pongrut@gmail.com>
2021-02-09 19:29:39 +0700

    Update test_slam.sh

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+#xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm  -e   " roslaunch jetbot jetbot_world.launch world_file:="$PWD"/src/worlds/pongrut.world " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+
+#xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+xterm  -e  " roslaunch jetbot jetbot_gmapping.launch " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+
+#xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+xterm  -e  " roslaunch jetbot jetbot_view_navigation.launch " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch "
+
+#xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch "
+xterm  -e  " roslaunch jetbot teleop.launch "
