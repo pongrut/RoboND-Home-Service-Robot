c98ffc5a38d792aa99d3fc35961c0f99418d5cd3
Pongrut Palarpong <pongrut@gmail.com>
2021-02-09 21:46:09 +0700

    Update test_slam.sh

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == turtlebot ]]; then
     echo "Launching turtlebot..."
-    xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+    xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="$PWD"/src/worlds/pongrut.world " &
     sleep 5
     xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
     sleep 5
@@ -18,4 +18,6 @@ else
     xterm  -e  " roslaunch jetbot jetbot_view_navigation.launch " &
     sleep 5
     xterm  -e  " roslaunch jetbot teleop.launch "
+
 fi
+
