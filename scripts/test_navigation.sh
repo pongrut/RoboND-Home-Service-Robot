7a65ab4c83f86f54e21a3330d80a732e4f825fe2
Pongrut Palarpong <pongrut@gmail.com>
2021-02-09 21:48:59 +0700

    Update argument for robot selection

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,12 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-#xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
-xterm  -e   " roslaunch jetbot jetbot_world.launch world_file:="$PWD"/src/worlds/pongrut.world " &
-sleep 5
+if [[ $1 == turtlebot ]]; then
+    echo "Launching turtlebot..."
+    xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="$PWD"/src/worlds/pongrut.world " &
+    sleep 5
+    xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:="$PWD"/src/map/pongrut_map.yaml " &
+    sleep 5
+    xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch "
+else
+    echo "Launching jetbot..."
+    xterm  -e   " roslaunch jetbot jetbot_world.launch world_file:="$PWD"/src/worlds/pongrut.world " &
+    sleep 5
+    xterm  -e   " roslaunch jetbot jetbot_amcl.launch map_file:="$PWD"/src/map/pongrut_map.yaml " &
+    sleep 5
+    xterm  -e  " roslaunch jetbot jetbot_view_navigation.launch "
+fi
 
-#xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:="$PWD"/src/map/pongrut_map.yaml " &
-xterm  -e   " roslaunch jetbot jetbot_amcl.launch map_file:="$PWD"/src/map/pongrut_map.yaml " &
-sleep 5
-
-#xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch "
-xterm  -e  " roslaunch jetbot jetbot_view_navigation.launch "
